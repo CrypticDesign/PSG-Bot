3f6f6a68d9ced1603ad3f2ab0368127c41ca42d1
CrypticDesign <mashoobee@gmail.com>
2018-06-01 18:24:26 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,12 +1,15 @@
 const Discord = require('discord.js');
-const botconfig = require("./botconfig.json")
+const botconfig = require("./botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
   let helpEmbed = new Discord.RichEmbed()
   .setTitle("List of Commands")
   .setColor(botconfig.blue)
   .setDescription("Here is a list of all the commands available right now:")
-  .addField("help", "Gives you a list of commands")
+  .addField("help", "Gives you a list of commands");
+  
+  message.delete().catch(O_o => {}); 
+  message.channel.send(helpEmbed); 
 }
 
 module.exports.help = {
